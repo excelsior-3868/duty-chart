843fbd488fb24666669bbcfd877cc27cc1fdacc9
Siddhartha Aadi Bajracharya <excelsior-3868@excelsior.edu.np>
2026-02-04 20:37:53 +0545

    Delete backend/notification_service directory

--- backend/notification_service/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class NotificationServiceConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'notification_service'
-
-    def ready(self):
-        import notification_service.signals
